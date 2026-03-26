3f4c7ab5140e106141aefaef4de01f820efb67cb
john <a3137418@gmail.com>
2026-03-26 20:35:40 +0800

    0326

--- src/main/resources/static/js/book.js
+++ src/main/resources/static/js/book.js
@@ -19,22 +19,25 @@ const bookTableBody = document.getElementById("bookTableBody");
 
 // 抓取畫面元素(id)-新增表單相關
 const bookForm = document.getElementById("bookForm");
+const bookId = document.getElementById("bookId");
 const bookName = document.getElementById("bookName");
 const bookPrice = document.getElementById("bookPrice");
 const bookAmount = document.getElementById("bookAmount");
 const bookPub = document.getElementById("bookPub");
 const addBtn = document.getElementById("addBtn");
-const restBtn = document.getElementById("restBtn");
+const updateBtn = document.getElementById("updateBtn");
+const resetBtn = document.getElementById("resetBtn");
 
-
-
+// 紀錄目前表單模式
+// create = 新增模式
+// update = 修改模式
+let formMode = "create";
 
 // 綁定查詢按鈕事件
 findOneBtn.addEventListener("click", findBookById);
 findAllBtn.addEventListener("click", findAllBooks);
 addBtn.addEventListener("click", addBook);
-
-
+updateBtn.addEventListener("click", updateBook);
 
 // 顯示訊息
 function showMessage(message, type = "info") {
@@ -53,92 +56,194 @@ async function handleResponse(response) {
 	return result;
 }
 
-//表單新增的資料
-function getBookFormDataCreate(){
-	return 	{
-		name:bookName.value.trim(),
-		price:bookPrice.value ? Number(bookPrice.value) : null,
-		amount:bookAmount.value ? Number(bookAmount.value) : null,
-		pub:bookPub.checked
-	};
+//將資料帶回表單
+function fillForm(book){
+	bookId.value = book.id ?? "";
+	bookName.value = book.name ?? "";
+	bookPrice.value = book.price ?? "";
+	bookAmount.value = book.amount ?? "";
+	bookPub.checked = book.pub ?? false;
 }
 
 
-//新增書籍
-async function addBook(){
-	try{
-		const book = getBookFormDataCreate();
-		if(!book.name || book.price === null || book.amount === null){
-			showMessage("請輸入完整書名,價格,數量","error");
+
+
+// 表單新增的資料
+function getBookFormDataForCreate() {
+	return 	{
+		name: bookName.value.trim(),
+		price: bookPrice.value ? Number(bookPrice.value) : null,
+		amount: bookAmount.value ? Number(bookAmount.value) : null,
+		pub: bookPub.checked
+	};
+}
+
+// 表單修改的資料
+function getBookFormDataForUpdate() {
+	return 	{
+		id: bookId.value ? Number(bookId.value) : null,
+		name: bookName.value.trim(),
+		price: bookPrice.value ? Number(bookPrice.value) : null,
+		amount: bookAmount.value ? Number(bookAmount.value) : null,
+		pub: bookPub.checked
+	};
+}
+
+// 新增書籍
+async function addBook() {
+	try {
+		
+		const book = getBookFormDataForCreate();
+		
+		if(!book.name || !book.price === null || book.amount === null) {
+			showMessage("請輸入完整書名, 價格, 數量", "error");
 			return;
 		}
 		
-	
-		
-		const response = await fetch(API_BASE_URL,{
-			method:"POST",
-			headers:{
-				"Content-Type":"application/json"
+		const response = await fetch(API_BASE_URL, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json"
 			},
-			body:JSON.stringify(book)
+			body: JSON.stringify(book)
 		});
 		
 		const result = await handleResponse(response);
-		console.log(result);
-		showMessage(result.message || "新增成功","success");
-		bookForm.reset();//清空表單
-		findAllBooks();//重新查詢所有書籍
 		
-	}catch(error){
+		console.log(result);
+		showMessage(result.message || "新增成功", "success");
+		
+		bookForm.reset(); // 清空表單
+		findAllBooks(); // 重新查詢所有書籍
+		
+	} catch(error) {
 		showMessage(error.message, "error");
 	}
 }
 
-
-
+// 修改書籍
+async function updateBook() {
+	try {
+		
+		const book = getBookFormDataForUpdate();
+		
+		if(!book.id) {
+			showMessage("修改時必須要有 ID", "error");
+			return;
+		}
+		
+		if(!book.name || book.price === null || book.amount === null) {
+			showMessage("請輸入完整的書名,價格,數量", "error");
+			return;
+		}
+		
+		const response = await fetch(`${API_BASE_URL}/${book.id}`, {
+			method: "PUT",
+			headers: {
+				"Content-type": "application/json"
+			},
+			body: JSON.stringify(book)
+		});
+		
+		const result = await handleResponse(response);
+		
+		showMessage(result.message || "修改成功", "success");
+		singleResult.textContent = JSON.stringify(result.data);
+		findAllBooks();
+		
+	} catch(err) {
+		showMessage(error.message, "error");
+	}
+}
 
 // 查詢全部
 async function findAllBooks() {
 	try {
 		const response = await fetch(API_BASE_URL);
 		const result = await handleResponse(response);
-		console.log(result);//debug用
-		renderBookTable(result.data);//資料渲染到表格中
-		showMessage(result.message || "查詢全部成功","success");
-		
+		console.log(result); // debug 用
+		renderBookTable(result.data); // 資料渲染到表格中
 		showMessage(result.message || "查詢全部成功", "success");
 	} catch(error) {
-		renderBookTable([]);//資料渲染控拔資料
+		renderBookTable([]); // 資料渲染空白資料
 		showMessage(error.message, "error");
 	}
 }
 
 // 查詢單筆
 async function findBookById() {
-	try{
+	try {
 		const id = searchId.value;
-		if(!id){
-			showMessage("請輸入要查詢的書籍ID","error")
+		
+		if(!id) {
+			showMessage("請輸入要查詢的書籍 ID", "error");
 			return;
 		}
 		
 		const response = await fetch(`${API_BASE_URL}/${id}`);
 		const result = await handleResponse(response);
-		console.log(result);//debug用
-		console.log(JSON.stringify(result));//debug用 ，顯示json字串
 		
-		singleResult.textContent = JSON.stringify(result);
-		showMessage(result.message || "單筆查詢成功","success");
-	}catch(error){
+		console.log(result); // debug 用, 顯示 json 物件
+		console.log(JSON.stringify(result)); // debug 用, 顯示 json 字串
+		
+		singleResult.textContent = JSON.stringify(result); 
+		showMessage(result.message || "單筆查詢成功", "success");
+		fillForm(result.data);//將查到後的資料帶回表單
+	} catch(error) {
 		singleResult.textContent = "查詢失敗";
 		showMessage(error.message, "error");
 	}
 }
 
-//渲染表格
-function renderBookTable(books){
-	if(!books || books.length === 0){
-		bookTableBody.innerHTML=`
+//由表格載入資料到表單
+
+async function loadBookById(id){
+	try{
+		const response = await fetch(`${API_BASE_URL}/${id}`);
+		const result = await handleResponse(response);
+		
+		singleResult.textContent = JSON.stringify(result); 
+		showMessage(`已載入書籍 ID :${id}`,"success");
+		fillForm(result.data);//將查到後的資料帶回表單
+		
+	}catch(err){
+		showMessage(error.message, "error");
+	}
+}
+//刪除書籍
+async function deleteBookById(id){
+	
+	const isConfirmed = confirm("你確定要刪除書籍 ID : ${id} 嗎?");
+	
+	if(!isConfirmed){
+		showMessage("已取消刪除書籍 ID :${id}","info");
+		return;
+	}
+	
+	
+	try{
+		const response = await fetch(`${API_BASE_URL}/${id}`,{
+			method : "DELETE"
+		});
+		
+		const result = await handleResponse(response);
+		
+		showMessage(result.message || "刪除成功", "success");
+		findAllBooks();
+		
+		
+				
+	}catch(error){
+		singleResult.textContent = `刪除書籍 ID : ${id} 失敗`;
+		showMessage(error.message, "error");
+	}
+}
+
+
+// 渲染表格
+function renderBookTable(books) {
+	if(!books || books.length === 0) {
+		bookTableBody.innerHTML = `
 			<tr>
 				<td colspan="5" class="empty-now">目前沒有資料</td>
 			</tr>
@@ -150,34 +255,32 @@ function renderBookTable(books){
 	books.forEach(book => {
 		html += `
 			<tr>
-				<td>${book.id}</td>
+				<td onclick="loadBookById(${book.id})" style="cursor : pointer" title="按我一下即可修改資料">${book.id}</td>
 				<td>${escapeHtml(book.name)}</td>
 				<td>${book.price}</td>
 				<td>${book.amount}</td>
-				<td>${book.pub ? "是":"否"}</td>
+				<td>${book.pub ? "是" : "否"}</td>
+				<td>
+					<button onclick="loadBookById(${book.id})">修改</button>
+					<button onclick="deleteBookById(${book.id})" class="danger">刪除</button>
+				</td>
 			</tr>
 		`;
 	});
 	
-	bookTableBody.innerHTML = html;
-	
+	bookTableBody.innerHTML = html; 
 }
 
-//避免文字直接塞進HTML 提升安全性
-function escapeHtml(text){
-	if(text === null || text === undefined){
+// 避免文字直接塞進 html
+function escapeHtml(text) {
+	if(text === null || text === undefined) {
 		return "";
 	}
 	
 	return String(text)
-		.replaceAll("&","$amp;")
-		.replaceAll("<","$lt;")	
-		.replaceAll(">","$gt;")	
-		.replaceAll('"',"$quot;")	
-		.replaceAll("'","$#39;")	
-	
+			.replaceAll("&", "&amp;")
+			.replaceAll("<", "&lt;")
+			.replaceAll(">", "&gt;")
+			.replaceAll('"', "&quot;")
+			.replaceAll("'", "&#39;");
 }
-
-
-
-
