fc23d4705a08fb94ca6462d036b35acd353d3bb2
john <a3137418@gmail.com>
2026-04-23 20:43:20 +0800

    0423

---
+++ springboot-jpa-storybook/src/main/java/com/example/demo/repository/StoryBookRepository.java
@@ -0,0 +1,17 @@
+package com.example.demo.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.model.StoryBook;
+
+@Repository
+public interface StoryBookRepository extends JpaRepository<StoryBook , Integer>{
+	//依 id 由小到大排序
+	List<StoryBook> findAllByOrderByIdAsc();
+	
+	//由大到小
+	List<StoryBook> findAllByOrderByIdDesc();
+}
