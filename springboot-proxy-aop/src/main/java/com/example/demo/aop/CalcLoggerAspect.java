18d645696a9c9b863f832df1c9a264d992f8bbf1
john <a3137418@gmail.com>
2026-05-05 21:54:30 +0800

    0505_AOP

--- springboot-proxy-aop/src/main/java/com/example/demo/aop/CalcLoggerAspect.java
+++ springboot-proxy-aop/src/main/java/com/example/demo/aop/CalcLoggerAspect.java
@@ -5,6 +5,9 @@ import java.util.Arrays;
 import java.util.Date;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -41,4 +44,38 @@ public class CalcLoggerAspect {
 		//Log紀錄
 		System.out.printf("Log 前置通知[%s]: %s %s %n", dateTime , methodName, Arrays.toString(args));
 	}
+	
+	
+	
+	// 後置通知: 不論是否會發生例外都會執行
+	@After(value = "ptDiv()")
+	public void end(JoinPoint joinPoint) {
+		String methodName = joinPoint.getSignature().getName(); // 取得方法名稱
+		Object[] args = joinPoint.getArgs();
+		String dateTime = sdf.format(new Date());
+		//Log紀錄
+		System.out.printf("Log 後置通知[%s]: %s %s %n", dateTime , methodName, Arrays.toString(args));
+	}
+	
+	// 正常返回通知
+	@AfterReturning(value = "ptDiv()" , returning = "result")
+	public void afterReturning(JoinPoint joinPoint , Object result) {
+		String methodName = joinPoint.getSignature().getName(); // 取得方法名稱
+		Object[] args = joinPoint.getArgs();
+		String dateTime = sdf.format(new Date());
+		//Log紀錄
+		System.out.printf("Log 正常返回通知[%s]: %s %s 正常返回結果:%s %n", dateTime , methodName, Arrays.toString(args) , result);
+	}
+	
+	
+	// 異常返回通知
+	@AfterThrowing(value = "ptDiv()" , throwing = "ex")
+	public void afterThrowing(JoinPoint joinPoint , Throwable ex) {
+		String methodName = joinPoint.getSignature().getName(); // 取得方法名稱
+		Object[] args = joinPoint.getArgs();
+		String dateTime = sdf.format(new Date());
+		//Log紀錄
+		System.out.printf("Log 異常返回通知[%s]: %s %s 異常返回結果:%s %n", dateTime , methodName, Arrays.toString(args) , ex);
+	}
+	
 }
