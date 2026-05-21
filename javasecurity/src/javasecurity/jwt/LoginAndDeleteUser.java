60232a0994462ad7e164457dd210faa0f8315793
john <a3137418@gmail.com>
2026-05-21 20:21:21 +0800

    0521 JWT驗證

---
+++ javasecurity/src/javasecurity/jwt/LoginAndDeleteUser.java
@@ -0,0 +1,154 @@
+package javasecurity.jwt;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.security.auth.Subject;
+
+import com.nimbusds.jwt.JWTClaimsSet;
+
+import javasecurity.util.KeyUtil;
+
+/*
+ * 帳號密碼正確
+ * ↓
+ * 伺服器發一張 JWT 通行證
+ * ↓
+ * JWT 裡面寫著
+ * 使用者:admin
+ * 角色:ADMIN
+ * 權限:USER_DELETE
+ * ↓
+ * 呼叫刪除會員 API 時
+ * 伺服器檢查 JWT 是否有效
+ * ↓
+ * 再檢查有沒有 USER_DELETE 權限
+ * */
+public class LoginAndDeleteUser {
+	public static void main(String[] args) throws Exception {
+		// 1.產生 JWT 簽名密鑰
+		String signSecret = KeyUtil.generateSecret(32);
+		
+		// 2.模擬登入(成功之後會得到 token)
+		String token =  login("admin", "1234", signSecret);
+		if(token == null) {
+			System.out.println("燈入失敗");
+			return;
+		}
+		
+		System.out.printf("登入成功，取得 JWT: %s%n" , token);
+		System.out.println("\n============================");
+		
+		// 3.模擬呼叫刪除會員API
+		deleteUserApi(token, signSecret);
+		
+		System.out.println("\n============================");
+		// 4. 模擬呼叫新增會員API
+		addUserApi(token, signSecret);
+		
+		// 5. 模擬 10秒延遲
+		for(int i=1;i<=10;i++) {
+			Thread.sleep(1000);
+			System.out.println(i + " ");
+		}
+		System.out.println();
+		
+		//重新調用API
+		deleteUserApi(token, signSecret);
+		addUserApi(token, signSecret);
+	}
+	
+	
+	// 刪除會員API
+	public static void deleteUserApi(String token , String signingSecret) throws Exception{
+		System.out.println("呼叫API: 刪除會員");
+		System.out.println("需要權限: DELETE_USER");
+		
+		// 驗證 JWT 並取得 payload
+		boolean check = KeyUtil.verifyJWTSignature(token, signingSecret);
+		if(!check) {
+			System.err.println("JWT 驗證失敗");
+			return;
+		}
+		System.out.println("JWT 驗證成功");
+		
+		
+		JWTClaimsSet claims = KeyUtil.getClaimsFromToken(token);
+		String subject = claims.getSubject();
+		String role = claims.getStringClaim("role");
+		List<String> permissions = claims.getStringListClaim("permissions");
+		
+		System.out.printf("使用者: %s%n" , subject);
+		System.out.printf("角色: %s%n" , role);
+		System.out.printf("權限: %s%n" , permissions);
+		
+		// 檢查全縣是否足夠
+		if(permissions.contains("USER_DELETE")) {
+			System.out.println("授權成功: 可以刪除會員");
+		}else {
+			System.err.println("授權失敗: 權限不足，不可刪除會員");
+		}
+	}
+	
+	// 新增會員API
+		public static void addUserApi(String token , String signingSecret) throws Exception{
+			System.out.println("呼叫API: 刪除會員");
+			System.out.println("需要權限: USER_ADD");
+			
+			// 驗證 JWT 並取得 payload
+			boolean check = KeyUtil.verifyJWTSignature(token, signingSecret);
+			if(!check) {
+				System.err.println("JWT 驗證失敗");
+				return;
+			}
+			System.out.println("JWT 驗證成功");
+			
+			
+			JWTClaimsSet claims = KeyUtil.getClaimsFromToken(token);
+			String subject = claims.getSubject();
+			String role = claims.getStringClaim("role");
+			List<String> permissions = claims.getStringListClaim("permissions");
+			
+			System.out.printf("使用者: %s%n" , subject);
+			System.out.printf("角色: %s%n" , role);
+			System.out.printf("權限: %s%n" , permissions);
+			
+			// 檢查全縣是否足夠
+			if(permissions.contains("USER_ADD")) {
+				System.out.println("授權成功: 可以新增會員");
+			}else {
+				System.err.println("授權失敗: 權限不足，不可新增會員");
+			}
+		}
+	
+	
+	
+	public static String login(String username , String password , String signingSecret) throws Exception{
+		// 帳密判斷
+		if(!"admin".equals(username) || !"1234".equals(password)) {
+			return null;
+		}
+		
+		// 設定有效時間
+		Date now = new Date();
+		// JWT 10秒後過期
+		Date expireTime = new Date(now.getTime() + (10 * 1000L));
+		
+		
+		
+	// 建立 JWT payload
+		JWTClaimsSet claimSet = new JWTClaimsSet.Builder()
+				.subject(username)
+				.issuer("https://my-system.com")
+				.issueTime(now) // 發行時間
+				.expirationTime(expireTime) // 過期時間
+				.claim("role", "ADMIN")
+//				.claim("permission", "USER_DELETE")
+				.claim("permissions", List.of("USER_DELETE" , "USER_ADD"))
+				.build();
+		
+		return KeyUtil.signJWT(claimSet, signingSecret);
+		
+		
+	}
+}
---
+++ javasecurity/src/javasecurity/jwt/SimpleJWT.java
@@ -0,0 +1,62 @@
+package javasecurity.jwt;
+
+import java.text.ParseException;
+
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jwt.JWTClaimsSet;
+
+import javasecurity.util.KeyUtil;
+
+/*
+ * 建立一個 JWT 令牌
+ * 驗證JWT令牌
+ * */
+public class SimpleJWT {
+	public static void main(String[] args) throws JOSEException, ParseException {
+		// 1. 生成簽名密鑰
+		// JWK: 產生簽名用的密鑰(32 bytes)
+		String signinSecret = KeyUtil.generateSecret(32);
+		System.out.printf("密鑰: %s%n" , signinSecret);
+		
+		// 2.創建 JWT 的聲明(claim)
+		// JWT: 這是我們要進行簽名的部分(資料主體)
+		JWTClaimsSet claimsSet = new JWTClaimsSet.Builder()
+				.subject("iot") // 主題
+				.issuer("htpp://iot.com") //發行單位
+				.claim("id", "001") // 自訂聲明內容
+				.claim("name", "John")
+				.claim("dept", "IT")
+				.claim("floor", "7")
+				.claim("tel", "#212")
+				.build();
+		System.out.printf("Payload: %s%n" , claimsSet);
+		
+		// 3.進行簽名(將 claimSet 進行簽名) 得到 token(JWT)
+		String token = KeyUtil.signJWT(claimsSet, signinSecret);
+		System.out.printf("Token(JWT):　%s%n" , token);
+		
+		
+		System.out.println("\n=================================");
+		
+		// 4.驗證 token(JWT)
+		boolean check = KeyUtil.verifyJWTSignature(token, signinSecret);
+		
+		if(check) {
+			System.out.println("驗證成功");
+			// 讀取 token 中的 Poyload
+			JWTClaimsSet claims = KeyUtil.getClaimsFromToken(token);
+			System.out.printf("讀取subject: %s%n" , claims.getSubject());
+			System.out.printf("讀取issure: %s%n" , claims.getIssuer());
+			System.out.printf("讀取id: %s%n" , claims.getStringClaim("id"));
+			System.out.printf("讀取name: %s%n" , claims.getStringClaim("name"));
+			System.out.printf("讀取dept: %s%n" , claims.getStringClaim("dept"));
+			System.out.printf("讀取floor: %s%n" , claims.getStringClaim("floor"));
+			System.out.printf("讀取tel: %s%n" , claims.getStringClaim("tel"));
+		}else {
+			System.out.println("驗證失敗");
+		}
+		
+		
+		
+	}
+}
