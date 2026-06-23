aedf2b51e6be86f41449f5f5a0d4abafdc807744
john <a3137418@gmail.com>
2026-06-23 21:21:13 +0800

    0623

---
+++ frontend-rental/src/pages/AdminReservationsPage.jsx
@@ -0,0 +1,9 @@
+function AdminReservationsPage(){
+    return(
+        <section>
+            <h1>預約審核</h1>
+            <p>管理者可以在這裡核准或退回預約</p>
+        </section>
+    )
+}
+export default AdminReservationsPage;
