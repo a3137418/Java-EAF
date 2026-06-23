e9373ceb95b3dbac3250dd7243473319781a5911
john <a3137418@gmail.com>
2026-06-23 21:29:00 +0800

    0623 router指令安裝

--- frontend-rental/src/App.jsx
+++ frontend-rental/src/App.jsx
@@ -1,3 +1,14 @@
+/*
+路徑: /                   模組:<ItemsPage>             租用項目
+路徑: /login              模組:<LoginPage>             登入項目
+路徑: /my-reservations    模組:<MyReservationPage>     我的預約
+路徑: /admin/items        模組:<AdminItemsPage>        租用項目管理
+路徑: /admin/reservations 模組:<AdminReservationsPage> 預約審核
+
+安裝 React Router
+指令 npm install react-router-dom
+
+*/
 function App(){
   return(
     <div>
